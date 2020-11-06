777936a0bcb8aba74bf283c895b90bd9703d7a93
ClanZai <74037712+ClanZai@users.noreply.github.com>
2020-11-06 14:30:53 +0800

    Update 3.login.php

--- 3.login.php
+++ 3.login.php
@@ -1,6 +1,6 @@
 <?php 
     if (($_GET[id] == "john") && ($_GET[pwd]=="john1234"))
-        echo "Welcome";
+        echo "Welcome";                         #登入成功，顯示Welcome
     else
-        echo "fail login";
+        echo "fail login";                     #登入失敗
 ?>
